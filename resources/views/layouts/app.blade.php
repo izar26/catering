94337c52a13f25af107c7e7495f32cdfd3a19c59
izar1008 <kn1707412@gmail.com>
2025-07-10 10:02:27 +0700

    final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -166,7 +166,7 @@
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://www.instagram.com/markmhbr" target="_blank">Markmhbr</a>
+        {{-- Designed by <a href="https://www.instagram.com/markmhbr" target="_blank">Markmhbr</a> --}}
       </div>
     </div>
 
